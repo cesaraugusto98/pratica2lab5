58dcf0bceee52e0d7ff514791f5747a99b864d67
Cesar Augusto Siqueira Santos <cesaraugusto.santos@outlook.com>
2019-02-16 12:39:05 -0200

    Criacao do projeto

---
+++ pratica2CesarSantos/src/main/java/com/fatec/sp/ex2/pratica2CesarSantos/Pratica2CesarSantosApplication.java
@@ -0,0 +1,14 @@
+package com.fatec.sp.ex2.pratica2CesarSantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratica2CesarSantosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratica2CesarSantosApplication.class, args);
+	}
+
+}
+
---
+++ pratica2CesarSantos/src/test/java/com/fatec/sp/ex2/pratica2CesarSantos/Pratica2CesarSantosApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fatec.sp.ex2.pratica2CesarSantos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Pratica2CesarSantosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
